9381d2614194df09b366dc8d347df53e49e561a5
Syed3055 <sajidashaik142@gmail.com>
2025-09-30 16:06:04 +0530

    Committing 4 items from workspace ad4d990e-46f9-42ce-9c19-217e75f64575

---
+++ Fabric_Deployment/Ware_8897.Warehouse/dbo/Views/master_8899.sql
@@ -0,0 +1,2 @@
+-- Auto Generated (Do not modify) CD5FD199C0F31CD4241660A122D33D22A50C12DDA378BCAE694CADE87EC29BF1
+create view master_8899 as select * from [Lake_8899].[dbo].[master]
---
+++ Fabric_Deployment/Ware_8897.Warehouse/dbo/Views/store_view_8899.sql
@@ -0,0 +1,2 @@
+-- Auto Generated (Do not modify) 79A1202B927AADEE147B2DF7CD7539A5A380B013D6E9CAEAFF5DE5951E27DA60
+CREATE   view store_view_8899 as select * from [Lake_8899].[dbo].[store];
